dcbfa78308807914c35fb7143aaa0e0dcd9f7bf9
Michael M Kuhinica <taverneiro@gmail.com>
2015-08-04 18:01:45 +0700

    Generate derived key from HD Wallet using sha32uri as proposed by BitID BIP, create and sign the message with bitcoinlibjs instead of bitcore

--- www/js/services/bitIDService.js
+++ www/js/services/bitIDService.js
@@ -4,7 +4,7 @@ angular.module('copayApp.services')
   .factory('bitIDService', function(profileService, bwcService, addressParser, $http) {
     var service = {};
 
-    service.generateSignature = function() {
+    service.generateSignatureMessage = function() {
       var fc = profileService.focusedClient;
       if (fc.isPrivKeyEncrypted()) {
         profileService.unlockFC(function(err) {
@@ -17,24 +17,21 @@ angular.module('copayApp.services')
         });
         return;
       };
+
       var utils = bwcService.getUtils();
       var HDPrivateKey = utils.Bitcore.HDPrivateKey;
-      var HDPublicKey = utils.Bitcore.HDPublicKey;
-      var PrivateKey = utils.Bitcore.PrivateKey;
       var retrieved = new HDPrivateKey(fc.credentials.xPrivKey);
-      var derivedByArgument = retrieved.derive("m/0'/0xb11e'/489237833/0");
+      var sha256URL = Bitcoin.crypto.sha256(addressParser.getBitIDSiteURI());
+      var sha32uri = sha256URL.readInt32LE(1);
+      var derivedByArgument = retrieved.derive("m/0'/0xb11e'/"+sha32uri+"/0");
       var derPriv = derivedByArgument.privateKey;
-      console.log("Chave privada", derPriv);
-      // console.log(utils.Bitcore.crypto.Hash.sha256(addressParser.getBitIDURI()));
+      var derivedWIF = derPriv.toWIF();
+      var keyPair = Bitcoin.ECKey.fromWIF(derivedWIF);
       var message = addressParser.getMessageToSign();
-      var signed = utils.signMessage(message, derPriv);
-      var pubKey = derivedByArgument.publicKey.toString();
-      console.log("Chave pública", derivedByArgument.publicKey.toObject());
-      console.log("Signed Message", signed);
-      var verif = utils.verifyMessage(message, signed, derivedByArgument.publicKey);
-      console.log("Verified: ", verif);
-      var fullMessage = service.createMessage(signed, pubKey);
-      console.log(fullMessage);
+      var signedMessage = Bitcoin.Message.sign(keyPair, message);
+      var signed = signedMessage.toString('base64');
+      var pubKeyAddress = keyPair.pub.getAddress().toBase58Check();
+      var fullMessage = service.createMessage(signed, pubKeyAddress);
       return signed;
     };
 
