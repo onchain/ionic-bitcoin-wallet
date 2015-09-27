f2aa2de08ca5f3896e8cd346be6335e32c852edd
Michael M Kuhinica <taverneiro@gmail.com>
2015-09-27 17:37:23 +0800

    Updated angular-bitcore-wallet

--- www/lib/angular-bitcore-wallet-client/index.js
+++ www/lib/angular-bitcore-wallet-client/index.js
@@ -1,7 +1,7 @@
 var bwcModule = angular.module('bwcModule', []);
 var Client = require('bitcore-wallet-client');
 
-bwcModule.constant('MODULE_VERSION', '0.0.26');
+bwcModule.constant('MODULE_VERSION', '0.4.4');
 
 bwcModule.provider("bwcService", function() {
   var provider = {};
